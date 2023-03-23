74ea84dbe0e5881db5b09cab3a2828251b56e398
Keyur Bisetty <keyur@Keyurs-MacBook-Pro.local>
2023-03-23 01:49:58 -0400

    updated server.js with all necessary packages

--- server.js
+++ server.js
@@ -1,37 +1,43 @@
-
 const express = require('express');
 const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const htmlRoutes = require('./controllers/htmlRoutes');
-
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// Set up sessions
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-const hbs = exphbs.create({ helpers: require('./utils/helpers') });
+app.use(session(sess));
+
+// Set up Handlebars
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    store: new SequelizeStore({
-      db: sequelize,
-    }),
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+// Set up middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Use routes
 app.use(routes);
 app.use(htmlRoutes);
 
+// Start the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
 });
